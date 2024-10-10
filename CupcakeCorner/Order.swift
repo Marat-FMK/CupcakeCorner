c1599f7facce928a52472505e0b07cd1fbd5b19e
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-11 01:22:29 +0300

    class logic

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -5,4 +5,65 @@
 //  Created by Marat Fakhrizhanov on 10.10.2024.
 //
 
-import Foundation
+import SwiftUI
+
+@Observable
+class Order: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAdress = "streetAdress"
+        case _zip = "zip"
+    }
+    
+    static let types = ["Vanilla", "Strawberry", "Chokolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+    
+    var name = ""
+    var streetAdress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAdress: Bool {
+        if name.isEmpty||streetAdress.isEmpty||city.isEmpty||zip.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    var cost: Decimal {
+        
+        var cost = Decimal(quantity) * 2
+        
+        cost += Decimal(type) / 2
+        
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+        
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+        
+        return cost
+    }
+}
