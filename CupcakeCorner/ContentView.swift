8c6d8180226de55fea9e7463c3fcfbd5a6051060
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-11 01:23:01 +0300

    logic data flow

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,10 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var order = Order()
+    
     var body: some View {
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in:  3...20)
+                }
+                
+                Section {
+                    Toggle("Any special reguests?", isOn: $order.specialRequestEnabled.animation())
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink("Delivery details") {
+                        AdressView(order: order)
+                    }
+                }
+                
+            }
+            .navigationTitle("Capcake Corner")
+        }
         
     }
+    
 }
 
 #Preview {
