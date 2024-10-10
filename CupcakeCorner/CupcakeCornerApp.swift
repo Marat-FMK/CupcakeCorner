21b5b130d3408c90f6b54123cbdcab4f45a869cf
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-10 17:58:08 +0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Marat Fakhrizhanov on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
